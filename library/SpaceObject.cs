1fed33bedfcd448f91415a80f20d53c899f39aa1
Kjetil Berg <Kjetil.a.berg@hotmail.com>
2023-03-11 17:18:45 +0100

    Add project files.

---
+++ library/SpaceObject.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceSim
+{
+    public class SpaceObject
+    {
+
+        protected String name;
+
+        public SpaceObject(string name)
+        {
+            this.name = name;
+        }
+        public virtual void Draw()
+        {
+            Console.WriteLine(name);
+        }
+    }
+
+    public class Star : SpaceObject
+    {
+        public Star(String name) : base(name) { }
+        public override void Draw()
+        {
+            Console.Write("Star  : ");
+            base.Draw();
+        }
+    }
+
+    public class Planet : SpaceObject
+    {
+        public Planet(String name) : base(name) { }
+        public override void Draw()
+        {
+            Console.Write("Planet: ");
+            base.Draw();
+        }
+    }
+
+    public class Moon : SpaceObject
+    {
+        public Moon (String name) : base(name) { }
+        public override void Draw()
+        {
+            Console.Write("Moon  : ");
+            base.Draw();
+        }
+    }
+}
