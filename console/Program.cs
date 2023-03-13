00ff14d2e17625cb5fb606a1f555e154d1b9d8a5
Kjetil Berg <Kjetil.a.berg@hotmail.com>
2023-03-13 17:48:36 +0100

    Added search by alias

--- console/Program.cs
+++ console/Program.cs
@@ -14,10 +14,10 @@ class Astronomy
 
         while (obj == null)
         {
-            Console.WriteLine("Please input the name of a Sun, Planet or Moon:");
+            Console.WriteLine("Please input the name or alias of a Sun, Planet or Moon:");
             string? name = Console.ReadLine();
             if (name == null) continue;
-            obj = solarSystem.Find(obj => obj.Metadata.Name == name);
+            obj = solarSystem.Find(obj => obj.Metadata.Name.ToUpper() == name.ToUpper() || obj.Metadata.AKA.ToUpper() == name.ToUpper());
             if (obj == null) Console.WriteLine("Name not recognized, try another name.\n");
         }
 
@@ -32,6 +32,7 @@ class Astronomy
 
         obj.DrawInfo(days);
 
+        Console.WriteLine("\nPress enter to exit.");
         Console.ReadLine();
 
     }
